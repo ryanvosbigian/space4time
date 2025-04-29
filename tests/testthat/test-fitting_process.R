6715aa4d6ca19fc3916ac8864447d79816f6191a
ryanvosbigian <ryanvosbigian@gmail.com>
2025-04-29 12:23:36 -0700

    fix test fitting

--- tests/testthat/test-fitting_process.R
+++ tests/testthat/test-fitting_process.R
@@ -28,18 +28,30 @@ test_that("models can fit rstan", {
 
   sim.dat <- simulate_data(N = 800)
 
-  suppressMessages(ch <- s4t_cjs_ch(ch_df = sim.dat$ch_df,
-                   aux_age_df = sim.dat$aux_age_df,
-                   min_a = sim.dat$min_a,
-                   max_a = sim.dat$max_a,
-                   sites_names = sim.dat$sites_names,
-                   sites_config = sim.dat$sites_config,
-                   holdover_config = sim.dat$holdover_config))
+  suppressMessages(
+    ch <- s4t_cjs_ch(
+      ch_df = sim.dat$ch_df,
+      aux_age_df = sim.dat$aux_age_df,
+      min_a = sim.dat$min_a,
+      max_a = sim.dat$max_a,
+      sites_names = sim.dat$sites_names,
+      sites_config = sim.dat$sites_config,
+      holdover_config = sim.dat$holdover_config
+    )
+  )
 
-  expect_no_error(suppressMessages(summary(fit_s4t_cjs_rstan(p_formula = ~ t,
-                                    theta_formula = ~ a1*a2*s*j,
-                                    ageclass_formula = ~ FL,
-                                    fixed_age = TRUE,
-                                    s4t_ch = ch,chains = 2,warmup = 200,iter = 400))))
+  expect_no_error(suppressMessages(summary(
+    fit_s4t_cjs_rstan(
+      p_formula = ~ t,
+      theta_formula = ~ a1 *
+        a2 * s * j,
+      ageclass_formula = ~ FL,
+      fixed_age = TRUE,
+      s4t_ch = ch,
+      chains = 2,
+      warmup = 200,
+      iter = 400
+    )
+  )))
 
 })
